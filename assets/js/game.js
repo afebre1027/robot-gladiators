bf51b7c27941b399056fa226fed6c479ec06ee14
Andrew Febre <afebre1027@gmail.com>
2021-11-08 21:32:43 -0500

    revisiting fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?")
+console.log(playerName);
+console.log("this logs a string, good for leaving messages");
+console.log(10 +10);
+console.log("our robot's name is " + playerName);
+function fight () {
+window.alert("Te fight has begun!");
+}
+//fight();
